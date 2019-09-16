5cd7f7c798a55e5979d0255cec65cb73f218e0de
protolambda <proto@protolambda.com>
2019-09-16 14:29:04 -0400

    pubsub listener for transitions

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/protolambda/muskoka-worker
 
 go 1.12
+
+require cloud.google.com/go/pubsub v1.0.1
--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"bytes"
+	"cloud.google.com/go/pubsub"
+	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 	"os/exec"
 	"os/signal"
@@ -14,54 +16,95 @@ import (
 )
 
 var cliCmdName string
+var gcpProjectID string
+var subId string
 
 func main() {
-	var wait time.Duration
-	flag.DurationVar(&wait, "graceful-timeout", time.Second * 15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
 	flag.StringVar(&cliCmdName, "cli-cmd", "zcli transition blocks", "change the cli cmd to run transitions with")
+	flag.StringVar(&gcpProjectID, "gcp-project-id", "muskoka", "change the google cloud project to connect with pubsub to")
+	flag.StringVar(&subId, "sub-id", "poc-v0.8.3", "the pubsub subscription to listen for tasks from")
 	flag.Parse()
 
-	// TODO: start listening for pubsub events, filtered by spec-version
-	// TODO: download transition inputs
-	// TODO: run transition
-	// TODO: push transition results to API
+	mainContext, cancel := context.WithCancel(context.Background())
+
+	// Setup pubsub client
+	pubsubClient, err := pubsub.NewClient(mainContext, gcpProjectID)
+	if err != nil {
+		log.Fatalf("Failed to create pubsub client: %v", err)
+	}
+	sub := pubsubClient.Subscription(subId)
+	// check if the subscription exists
+	{
+		ctx, _ := context.WithTimeout(mainContext, time.Second*15)
+		if exists, err := sub.Exists(ctx, ); err != nil {
+			log.Fatalf("could not check if pubsub subscription exists")
+		} else if !exists {
+			log.Fatalf("subscription %s does not exist. Either the worker was misconfigured (try --sub-id) or a new subscription needs to be created and permissioned.", subId)
+		}
+	}
+	// configure pubsub receiver
+	sub.ReceiveSettings = pubsub.ReceiveSettings{
+		MaxExtension:           0, // triggers default
+		MaxOutstandingMessages: 20,
+		MaxOutstandingBytes:    1 << 10,
+		NumGoroutines:          4,
+		Synchronous:            false,
+	}
+	// try receiving messages
+	{
+		ctx, _ := context.WithTimeout(mainContext, time.Second*15)
+		if err := sub.Receive(ctx, func(ctx context.Context, message *pubsub.Message) {
+			var transitionMsg TransitionMsg
+			dec := json.NewDecoder(bytes.NewReader(message.Data))
+			if err := dec.Decode(&transitionMsg); err != nil {
+				log.Printf("failed to decode message JSON: %v (msg: %s)", err, message.Data)
+				message.Nack()
+				return
+			}
+			log.Printf("processing %s (%s)", transitionMsg.Key, transitionMsg.SpecVersion)
+			if err := transitionMsg.LoadFromBucket(); err != nil {
+				log.Printf("failed to load data from bucket for %s: %v", transitionMsg.Key, err)
+				message.Nack()
+				return
+			}
+			if err := transitionMsg.Execute(); err != nil {
+				log.Printf("failed to run transition for %s: %v", transitionMsg.Key, err)
+				message.Nack()
+				return
+			}
+			log.Printf("successfully processed transition: %s (%s)", transitionMsg.Key, transitionMsg.SpecVersion)
+			message.Ack()
+		}); err != nil {
+			log.Fatalf("failed to receive messages: %v", err)
+		}
+	}
 
 	c := make(chan os.Signal, 1)
 	// Catch SIGINT (Ctrl+C) and shutdown gracefully
 	signal.Notify(c, os.Interrupt)
 	<-c
-
-	// TODO shutdown pubsub client
+	cancel()
 	log.Println("shutting down")
 	os.Exit(0)
 }
 
-func loggingMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println(r.RequestURI)
-		next.ServeHTTP(w, r)
-	})
+type TransitionMsg struct {
+	Blocks      int    `json:"blocks"`
+	SpecVersion string `json:"spec-version"`
+	Key         string `json:"key"`
 }
 
-type TransitionRequest struct {
-	// key used in datastore, bucket prefix, etc.
-	key string
-	// count of blocks, for "block_%d.ssz"
-	blocks uint
-	// TODO time? other meta?
-}
-
-func (tr *TransitionRequest) LoadFromBucket() error {
+func (tr *TransitionMsg) LoadFromBucket() error {
 	// TODO
 	return nil
 }
 
-func (tr *TransitionRequest) Execute() {
+func (tr *TransitionMsg) Execute() error {
 	log.Println("executing request")
 	var args []string
-	args = append(args, "--pre", path.Join(tr.key, "pre.ssz"), "--post", path.Join(tr.key, "post.ssz"))
-	for i := uint(0); i < tr.blocks; i++ {
-		args = append(args, path.Join(tr.key, fmt.Sprintf("block_%d.ssz", i)))
+	args = append(args, "--pre", path.Join(tr.Key, "pre.ssz"), "--post", path.Join(tr.Key, "post.ssz"))
+	for i := 0; i < tr.Blocks; i++ {
+		args = append(args, path.Join(tr.Key, fmt.Sprintf("block_%d.ssz", i)))
 	}
 	// trigger CLI to run transition in Go routine
 	cmd := exec.Command(cliCmdName, args...)
